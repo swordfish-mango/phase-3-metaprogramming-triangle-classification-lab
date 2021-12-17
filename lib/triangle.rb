4168a5474f0d50e1a878a17a78b9549d41f392f4
Marcus <marcus.shimotsu@gmail.com>
2021-12-17 12:05:07 -0600

    commit tree 8c69eb2c2576636b6638d62b954dc317f764ccf8

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,29 @@
 class Triangle
-  # write code here
+  attr_accessor :a
+  attr_accessor :b
+  attr_accessor :c
+  
+
+  def initialize(a,b,c)
+    if a <= 0 || b<=0 || c<= 0 || c>= a+b || b >= c+a || a >= b+c
+      raise TriangleError
+    else
+      @a = a
+      @b = b
+      @c = c
+    end
+  end
+
+  def kind
+    if a==b && b==c
+      return :equilateral
+    elsif a==b || b==c || c==a
+      return :isosceles
+    else
+      return :scalene
+    end
+  end
+
+  class TriangleError < StandardError
+  end
 end
